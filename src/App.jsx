a6e82776b65a8629934bb3f29291e564ddd1c982
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 14:48:18 +0100

    routing added to Login and Weather pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Login from "./pages/Login/Login";
+import Weather from "./pages/Weather/Weather";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/weather" element={<Weather />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
