5fcc1316804efbbe1b7ff74a70f25f0bebb4dc36
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 20:39:38 +0100

    edit env variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -16,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-export default firebase;
+export default app;
