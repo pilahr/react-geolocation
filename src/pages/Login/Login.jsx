c6ff24969443ca8cd3fa7a398eb54d0fb3120980
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 16:04:09 +0100

    Created Header component and placed it on Login and Weather pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Login.scss";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import Header from "../../components/Header/Header";
 
 const Login = () => {
   return (
     <div className="login">
       <div>
-        <h2 className="login__text">Want to know what is going on outside?</h2>
-        <h4 className="login__text">Let's check how the weather is like today..</h4>
+        <Header />
       </div>
       <div>
+        <h2 className="login__text">Want to know what is going on outside?</h2>
+        <h4 className="login__text">
+          Let's check how the weather is like today..
+        </h4>
+      </div>
+      <div className="login__form">
         <LoginForm />
       </div>
     </div>
