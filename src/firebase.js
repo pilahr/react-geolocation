717d7b352ab02fcc0a315d544b7e508e53e2b71b
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 20:36:16 +0100

    edited code in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -15,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-export default app;
+export default firebase;
