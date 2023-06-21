ac23265f2a9f8b715bbc4ff79759e542e565282e
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 15:33:44 +0100

    created log in form component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Login.scss";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div>
+        <h2 className="login__text">Want to know what is going on outside?</h2>
+        <h4 className="login__text">Let's check how the weather is like today..</h4>
+      </div>
+      <div>
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
