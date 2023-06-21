6fa1a2af0366760deb471ef7ad96704320b333d8
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 20:11:22 +0100

    imported firebase to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,4 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-export default app;
+export default firebase.app;
