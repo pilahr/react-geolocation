43ccd1d37508f70ffff3d40bf12edd7fc300965a
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 20:13:00 +0100

    edit firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,16 +6,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_MESSAGING_APP_ID,
+  apiKey: "AIzaSyCZxHpsxZoL8hACQnt6nrlWwLKzcF2cdT0",
+  authDomain: "react-geolocation-project-01.firebaseapp.com",
+  projectId: "react-geolocation-project-01",
+  storageBucket: "react-geolocation-project-01.appspot.com",
+  messagingSenderId: "156511131324",
+  appId: "1:156511131324:web:8c20128c04e0b80e9f22b9",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-export default firebase.app;
+export default app;
