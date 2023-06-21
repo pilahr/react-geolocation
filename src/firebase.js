0c324961c0bce58c4e3cfb3417510d28786bb55f
Piyaporn <piyapornlahr@gmail.com>
2023-06-21 12:18:35 +0100

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZxHpsxZoL8hACQnt6nrlWwLKzcF2cdT0",
+  authDomain: "react-geolocation-project-01.firebaseapp.com",
+  projectId: "react-geolocation-project-01",
+  storageBucket: "react-geolocation-project-01.appspot.com",
+  messagingSenderId: "156511131324",
+  appId: "1:156511131324:web:8c20128c04e0b80e9f22b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
