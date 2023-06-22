90e577759c0cc7a428cc4f0514b4740d334456b9
Piyaporn <piyapornlahr@gmail.com>
2023-06-22 14:08:39 +0100

    Fetched data from API completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Login from "./pages/Login/Login";
@@ -6,7 +6,35 @@ import Weather from "./pages/Weather/Weather";
 
 const App = () => {
   const [user, setUser] = useState();
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("london");
 
+  const getWeatherData = async (city) => {
+    const apiKey = `${process.env.REACT_APP_WEATHER_API_KEY}`;
+
+    try {
+      let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}`;
+
+      const response = await fetch(
+        url + `&q=${city[0].toUpperCase() + city.slice(1)}&days=7&aqi=yes`
+      );
+
+      if (!response.ok) {
+        throw new Error("Sorry something went wrong!");
+      }
+
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherData(city);
+  }, [city]);
+
+  console.log(weatherData);
   return (
     <>
       <Router>
