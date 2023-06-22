e0cc2dfe11ea4e07db3edcc9cb35600171b9021b
Piyaporn <piyapornlahr@gmail.com>
2023-06-22 13:10:57 +0100

    Completed testing the environment variables

--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -3,15 +3,14 @@ import "./Weather.scss";
 import Header from "../../components/Header/Header";
 
 const Weather = () => {
-  console.log(process.env.REACT_APP_SECRET_KEY);
+
   return (
     <div className="weather">
       <div>
         <Header />
       </div>
       <div>
-        <h1>Hello this is weather page</h1>
-        <p>Our secret key is {process.env.REACT_APP_SECRET_KEY}</p>
+
       </div>
     </div>
   );
