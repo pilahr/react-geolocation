5a3836e722d3bf25b745aa2fe01a500a8a4e3fd0
Piyaporn <piyapornlahr@gmail.com>
2023-06-22 13:01:34 +0100

    changed script for build in package.json

--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -3,6 +3,7 @@ import "./Weather.scss";
 import Header from "../../components/Header/Header";
 
 const Weather = () => {
+  console.log(process.env.REACT_APP_SECRET_KEY);
   return (
     <div className="weather">
       <div>
@@ -11,7 +12,6 @@ const Weather = () => {
       <div>
         <h1>Hello this is weather page</h1>
         <p>Our secret key is {process.env.REACT_APP_SECRET_KEY}</p>
-        console.log(process.env.REACT_APP_SECRET_KEY);
       </div>
     </div>
   );
