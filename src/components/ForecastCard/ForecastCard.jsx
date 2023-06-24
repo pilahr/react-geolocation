95312a6e9ab5105afcd9559d5de853bf1a130192
Piyaporn <piyapornlahr@gmail.com>
2023-06-24 20:41:42 +0100

    Completed Adding Air Quality on Weather page

--- src/components/ForecastCard/ForecastCard.jsx
+++ src/components/ForecastCard/ForecastCard.jsx
@@ -1,33 +1,12 @@
 import React from "react";
 import "./ForecastCard.scss";
 
-const ForecastCard = ({ day }) => {
-  const dayOfTheWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const date = new Date(day.date);
-
+const ForecastCard = ({ result, label, icon }) => {
   return (
     <div className="forecastCard">
-      <div className="forecastCard__wrap">
-        <h3 className="forecastCard__header">{dayOfTheWeek[date.getDay()]}</h3>
-        <div className="forecastCard__blog">
-          <div className="forecastCard__blog--icon">
-            <img src={day.day.condition.icon} alt="weather icon" />
-          </div>
-          <div className="forecastCard__blog--temp">
-            <h3 className="forecastCard__blog--temp-text">
-              {day.day.avgtemp_c} °C
-            </h3>
-          </div>
-        </div>
-      </div>
+      <h3 className="forecastCard__header">{label}</h3>
+      <img className="forecastCard__icon" src={icon} alt={icon} />
+      <h3 className="forecastCard__text">{result}</h3>
     </div>
   );
 };
--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.scss";
 import Header from "../../components/Header/Header";
 import SearchContainer from "../../container/SearchContainer/SearchContainer";
 import WeatherContainer from "../../container/WeatherContainer/WeatherContainer";
 import Button from "../../../src/components/Button/Button";
 import ForecastRow from "../../components/ForecastRow/ForecastRow";
+import ForecastCard from "../../components/ForecastCard/ForecastCard";
+import AirQuality from "../../assets/AirQuality.png";
 
-const Weather = ({ getUserLocation, weatherData, setLocation }) => {
+const Weather = ({ weatherData, setLocation }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [airQuality, setAirQuality] = useState("");
 
   const handleSearchInput = (event) => {
     event.preventDefault();
@@ -19,6 +22,29 @@ const Weather = ({ getUserLocation, weatherData, setLocation }) => {
   };
 
   const forecast = weatherData.forecast.forecastday;
+
+  // console.log(aqi);
+
+  const getAirQuality = () => {
+    const aqi = weatherData.current.air_quality["us-epa-index"];
+    if (aqi === 1) {
+      setAirQuality("Good");
+    } else if (aqi === 2) {
+      setAirQuality("Moderate");
+    } else if (aqi === 3) {
+      setAirQuality("Unhealthy for sensitive group");
+    } else if (aqi === 4) {
+      setAirQuality("Unhealthy");
+    } else if (aqi === 5) {
+      setAirQuality("Very Unhealthy");
+    } else if (aqi === 6) {
+      setAirQuality("Hazardous");
+    }
+  };
+
+  useEffect(() => {
+    getAirQuality();
+  });
 
   return (
     <div className="weather">
@@ -38,6 +64,15 @@ const Weather = ({ getUserLocation, weatherData, setLocation }) => {
         <div>
           <WeatherContainer weatherData={weatherData} />
         </div>
+        <div>
+          <ForecastCard
+            label="Air Quality"
+            icon={AirQuality}
+            airQuality={airQuality}
+            result={airQuality}
+          />
+        </div>
+
         <div className="weather__wrap--forecastCard">
           {forecast.map((day, index) => {
             return (
