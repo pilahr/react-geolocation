834b8931ad03dab3f55ffa373175b2ef92f31c1b
Piyaporn <piyapornlahr@gmail.com>
2023-06-27 10:44:14 +0100

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
   };
 
   const getWeatherData = async (location) => {
-    const apiKey = `${process.env.REACT_APP_WEATHER_API_KEY}`;
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
     try {
       let url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}`;
